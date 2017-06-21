9ed55ca05c1a4c120a5b530cd9bb12b25b1a3e46
Eduardo Pimenta Marrocos e Freitas <epimenta@gmail.com>
2017-06-20 21:30:23 -0300

    First commit in github

---
+++ src/com/pwepper/Pwepper.java
@@ -0,0 +1,45 @@
+package com.pwepper;
+
+import twitter4j.PagableResponseList;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class Pwepper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		// Filled auto-generated method stub
+		long people = 0;
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("njvydaA8JHdLzSU4aiYyw")
+		  .setOAuthConsumerSecret("ecgEjl6LoljJva74WGknIoPN42pln4cwmdsF4Efp44")
+		  .setOAuthAccessToken("25489798-4ExR3eQqaaUfq5bU0umYZDrqz7QPxBPWaot481bRn")
+		  .setOAuthAccessTokenSecret("lZ1pJ5m8TRYY4GsAI8WC0g1FNISArq2zQXWz0e68BRg");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		
+        try {
+            //Twitter twitter = new TwitterFactory().getInstance();
+            User user = twitter.verifyCredentials();
+            PagableResponseList following;
+            System.out.println("Successfully verified credentials of " + user.getScreenName());
+            System.out.println("Now for followers list:");
+            while (people < following.size()){
+            	following = twitter.getFollowersList(people, people);
+            	System.out.println(following.toString);
+            	            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to verify credentials: " + te.getMessage());
+            System.exit(-1);
+        }
+
+	}
+}
